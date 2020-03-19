c8dcf9afbf66a966994dfea3c032e6489d494fc7
Javier Rodger <test@test.com>
2020-03-19 11:12:15 +0100

    Update Register-FMNTAuthStore.ps1

--- ForestManagement/functions/ntauthstore/Register-FMNTAuthStore.ps1
+++ ForestManagement/functions/ntauthstore/Register-FMNTAuthStore.ps1
@@ -1,20 +1,44 @@
-﻿function Register-FMNTAuthStore
-{
+﻿function Register-FMNTAuthStore {
+	<#
+		.SYNOPSIS
+		Regisgter NTAuthStore certificates
+		
+		.DESCRIPTION
+		Regisgter NTAuthStore certificates
+		This is the ideal / desired state for the NTAuthStore certificate configuration.
+		Forests will be brought into this state by using Invoke-FMNTAuthStore.
+		
+		.PARAMETER Certificate
+		The certifcate to apply.
+		
+		.PARAMETER Authorative
+		Should the NTAuthStore configuration overwrite the existing configuration, rather than adding to it (default).
+
+		.EXAMPLE
+		Register-FMNTAuthStore -Certificate $NTAuthStoreCertificate
+
+		Register a certiciate.
+		
+		.EXAMPLE
+		Register-FMNTAuthStore -Authorative
+		
+		Register the autorative state.
+	#>
 	[CmdletBinding()]
 	Param (
-		
+		[Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = "Certificate")]
+		[System.Security.Cryptography.X509Certificates.X509Certificate2]
+		$Certificate,
+
+		[Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = "Authorative")]
+		[switch]
+		$Authorative		
 	)
 	
-	begin
-	{
-		
-	}
-	process
-	{
-	
-	}
-	end
-	{
-	
+	process {
+		switch ($pscmdlet.ParameterSetName) {
+			Certificate { $script:ntAuthStoreCertificates[$Certificate.Thumbprint] = $Certificate }
+			Authorative { $script:ntAuthStoreAuthorative = $Authorative.ToBool() }
+		}		
 	}
 }
